dfa2ef38ca0230495b92ddd923b52ec8c6bcae0a
Ankush Sharma <ankush20058@gmail.com>
2018-02-09 15:56:52 +0530

    Converting WinNotificationReq -> WinNotification

--- src/main/groovy/com/adsizzler/mangolaa/kafkaconsumer/wins/storage/kafka/KafkaListener.groovy
+++ src/main/groovy/com/adsizzler/mangolaa/kafkaconsumer/wins/storage/kafka/KafkaListener.groovy
@@ -2,6 +2,7 @@ package com.adsizzler.mangolaa.kafkaconsumer.wins.storage.kafka
 
 import com.adsizzler.mangolaa.kafkaconsumer.wins.storage.constants.KafkaTopics
 import com.adsizzler.mangolaa.kafkaconsumer.wins.storage.domain.WinNotification
+import com.adsizzler.mangolaa.kafkaconsumer.wins.storage.request.WinNotificationRequest
 import com.adsizzler.mangolaa.kafkaconsumer.wins.storage.service.WinNotificationService
 import com.adsizzler.mangolaa.kafkaconsumer.wins.storage.util.Gzip
 import com.adsizzler.mangolaa.kafkaconsumer.wins.storage.util.Json
@@ -45,10 +46,11 @@ class KafkaListener {
             // payload -> Decompress Gzip -> JSON -> POJO
             def payload = record.value()
             def json = Gzip.decompress(payload)
-            def winNotification = Json.toObject(json, WinNotification)
+            def winRequest = Json.toObject(json, WinNotificationRequest)
+            def win = new WinNotification(winRequest)
 
-            log.debug 'Win {}', winNotification
-            winNotificationService.save(winNotification)
+            log.debug 'Win {}', win
+            winNotificationService.save(win)
         }
         .exceptionHandler{ ex ->
             log.error '', ex
